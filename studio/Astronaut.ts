e2017cee08d4526822073f231af1ca5722e111f0
Pooja Bhatia <pooja08.bhatia@gmail.com>
2021-08-05 20:20:08 -0500

    completed

---
+++ studio/Astronaut.ts
@@ -0,0 +1,16 @@
+
+export class Astronaut {
+ 
+     massKg:number;
+     name: String;
+     constructor(massKg:number,name:string){
+      this.massKg = massKg;
+      this.name = name;
+     }
+    
+    // properties and methods
+ }
+
+ interface Payload{
+        
+}
